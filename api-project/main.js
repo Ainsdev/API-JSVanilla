253117b9fbc349d7c2aeb29006e49f5abb18de88
Ainsdev <ainsn1654@gmail.com>
2022-07-10 17:14:35 -0400

    master

---
+++ api-project/main.js
@@ -0,0 +1,43 @@
+// //button dark mode
+// const darkBtn = document.getElementById('dark_btn');
+// const html = document.getElementById('html');
+// darkBtn.addEventListener('click', () => {
+//     if (html.classList.contains('dark') == true) {
+//         html.classList.remove('dark');
+//         console.log('dark mode off');
+//     } else{
+//         html.classList.add('dark');
+//     }
+// });
+// //button dark mode
+window.onload = function async() {
+    document.getElementById('top100').classList.remove('hidden');
+}
+//btns
+const top100Btn = document.getElementById('top100_btn');
+const searchBtn = document.getElementById('search_btn');
+const historicalBtn = document.getElementById('historical_btn');
+//btns
+//contents
+const top100Cont = document.getElementById('top100_content');
+const searchCont = document.getElementById('search_content');
+const historicalCont = document.getElementById('historical_content');
+//contents
+//functions NAV
+top100Btn.addEventListener('click', () => {
+    document.getElementById('top100').classList.remove('hidden');
+    document.getElementById('search').classList.add('hidden');
+    document.getElementById('historical').classList.add('hidden');
+})
+searchBtn.addEventListener('click', () => {
+    document.getElementById('search').classList.remove('hidden');
+    document.getElementById('top100').classList.add('hidden');
+    document.getElementById('historical').classList.add('hidden');
+});
+historicalBtn.addEventListener('click', () => {
+    document.getElementById('historical').classList.remove('hidden');
+    document.getElementById('top100').classList.add('hidden');
+    document.getElementById('search').classList.add('hidden');
+})
+//functions NAV
+
--- api-project/tailwind.config.js
+++ api-project/tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1F2024',
+        secondary: '#3375EF',
+        tertiary: '#42A8F5',
+      }
+    },
   },
   plugins: [],
 }
